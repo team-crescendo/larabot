8ebbc3f8b58c809915417ed421ac5dc65c569971
Forte Staff <forte@crsd.team>
2020-08-30 01:29:49 +0900

    fix: 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ WORKSPACE=$(pwd)
 PID=$(ps -ef | grep "[p]ython3 ${WORKSPACE}/src/bot.py" | awk '{ print $2 }')
 
 if [ PID ]; then
-  echo "$PID" | xargs kill -INT
+  echo "$PID" | xargs kill -s SIGINT
 fi
 
 nohup python3 "${WORKSPACE}/src/bot.py" &
